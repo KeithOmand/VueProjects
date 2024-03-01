fe2c9d50a3b3c4731f40e740d4bfc6241a3e126d
Keith Omand <keith.omand@gmail.com>
2024-02-29 22:37:49 -0700

    added details, variants & sizes to main.js & rendered in index

--- VueProject1/main.js
+++ VueProject1/main.js
@@ -3,7 +3,13 @@ const app = Vue.createApp({
         return{
             product: 'Socks',
             image:'./assets/images/socks_green.jpg',
-            inventory:100
+            inStock:true,
+            details:['50% cotton', '30% wool', '20% polyester'],
+            variants:[
+                {id:2234, color:'green'},
+                {id:2235, color:'blue'}
+            ],
+            sizes:['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge', 'xxxlarge']
         }
     }
 })
