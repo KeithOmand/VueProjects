88512d4116b025c519efecc90703030183f54cc7
Keith Omand <keith.omand@gmail.com>
2024-03-01 18:27:54 -0700

    Added Color circles to represent the different color types, added "Add to Cart" button with full functionality, added a cart counter

--- VueProject1/main.js
+++ VueProject1/main.js
@@ -1,15 +1,34 @@
 const app = Vue.createApp({
-    data(){
-        return{
-            product: 'Socks',
-            image:'./assets/images/socks_green.jpg',
-            inStock:true,
-            details:['50% cotton', '30% wool', '20% polyester'],
-            variants:[
-                {id:2234, color:'green'},
-                {id:2235, color:'blue'}
-            ],
-            sizes:['xsmall', 'small', 'medium', 'large', 'xlarge', 'xxlarge', 'xxxlarge']
-        }
+  data() {
+    return {
+      cart: 0,
+      product: 'Socks',
+      brand:'Vue Mastery',
+      selectedVariant:0,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity:50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity:0 },
+      ],
+    };
+  },
+  methods: {
+    addToCart() {
+      this.cart++;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    },
+  },
+  computed:{
+    title(){
+        return this.brand + ' ' + this.product
+    },
+    image(){
+        return this.variants[this.selectedVariant].image
+    },
+    inStock(){
+        return this.variants[this.selectedVariant].quantity
     }
-})
+  }
+});
